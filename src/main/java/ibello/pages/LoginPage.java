5f1bd2331dd38f07a5fa6b0aeb9e742a91440aa4
Zoltán Kasza <zoltan.kasza@ibello.hu>
2024-09-03 22:43:04 +0200

    add Login pageobject

---
+++ src/main/java/ibello/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package ibello.pages;
+
+import hu.ibello.core.Name;
+import hu.ibello.elements.WebElement;
+import hu.ibello.pages.PageObject;
+import hu.ibello.search.By;
+import hu.ibello.search.Find;
+
+@Name("Login page")
+public class LoginPage extends PageObject {
+
+    private static final String URL = "https://ibello.hu/tasks/cases/#/login";
+
+    @Find(by = By.ID, using = "username")
+    private WebElement usernameField;
+
+    @Find(by = By.ID, using = "password")
+    private WebElement passwordField;
+
+    @Find(by = By.BUTTON_TEXT, using = "Bejelentkezés")
+    private WebElement loginButton;
+
+    public void open_demo_page() {
+        browser().openURL(URL);
+        browser().resize(1920, 1080);
+        browser().maximize();
+    }
+
+    public void page_must_be_loaded() {
+        expectations().expect(usernameField).toBe().displayed();
+        expectations().expect(passwordField).toBe().displayed();
+    }
+
+    public void click_on_login_button() {
+        doWith(loginButton).click();
+    }
+
+    public void set_username_to_$(String username) {
+        doWith(usernameField).setValue(username);
+    }
+
+    public void set_password_to_$(String password) {
+        doWith(passwordField).setValue(password);
+    }
+
+}
